dcb596f272b64af69e3c62d078b8924b4dd301e2
rudraraman498 <85647507+rudraraman498@users.noreply.github.com>
2021-06-10 02:34:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,10 +16,7 @@ def answer(loc):
     print(first)
     # if (first==1): loc = "howrah"
     api_key = False
-    # If you have a Google Places API key, enter it here
-    # api_key = 'AIzaSy___IDByT70'
-    # https://developers.google.com/maps/documentation/geocoding/intro
-
+    
     if api_key is False:
         api_key = 42
         serviceurl = 'http://py4e-data.dr-chuck.net/json?'
